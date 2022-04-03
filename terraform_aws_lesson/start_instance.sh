4b18a80d492ae632bcaa24d040e0a415cee54041
root <root@localhost.localdomain>
2022-04-03 16:28:22 -0400

    edit start file

--- terraform_aws_lesson/start_instance.sh
+++ terraform_aws_lesson/start_instance.sh
@@ -4,4 +4,4 @@ export AWS_ACCESS_KEY_ID=AKIA4PA7UO554VZXRAPE
 export AWS_SECRET_ACCESS_KEY=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
 echo "====================="
 
-terraform destroy
+terraform apply
