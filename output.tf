4f373d4602558c054b54da9cf3846c809be17c62
root <root@localhost.localdomain>
2022-04-03 23:49:36 -0400

    add output

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "info1" {
+  value = join(" - ",[docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].name])
+}
+
+output "info2" {
+  value = join(" - ",[docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].name])
+}
+
+output "all_info" {
+  value = [for i in docker_container.nodered_container[*]: join(":",[i.name],[i.ip_address],i.ports[*]["external"])]
+  description = "all_info: name, ip_address, ports"
+}
+
+
